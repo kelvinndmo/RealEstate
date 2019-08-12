cbfa20ee9f5790ec45aaeb028539cd36084a7f49
novak <kelvin.onkundi@andela.com>
2019-08-13 00:38:00 +0300

    update stuff

--- norah/settings.py
+++ norah/settings.py
@@ -83,16 +83,17 @@ WSGI_APPLICATION = 'norah.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': os.environ.get('DB_NAME'),
-#         'USER': os.environ.get('DB_USER'),
-#         'PASSWORD':os.environ.get('DB_PASSWORD'),
-#         'HOST':os.environ.get('DB_HOST')
-#     }
-# }
-DATABASES = {'default': dj_database_url.config(default=os.environ['POSTGRES_URL'])}
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get('DB_NAME', 'postgres'),
+        'USER': os.environ.get('DB_USER', 'postgres'),
+        'PASSWORD': os.environ.get('DB_PASSWORD', 'password'),
+        'HOST': os.environ.get('DB_HOST', 'localhost'),
+        'PORT': os.environ.get('DB_PORT', '5432'),
+    }
+}
+# DATABASES = {'default': dj_database_url.config(default=os.environ['POSTGRES_URL'])}
 
 
 # Password validation
