270c036cc29aa4904040c25904936868905b116b
novak <kelvin.onkundi@andela.com>
2019-08-12 23:46:31 +0300

    update stuff

--- norah/settings.py
+++ norah/settings.py
@@ -84,7 +84,7 @@ WSGI_APPLICATION = 'norah.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': os.environ.get('DB_NAME'),
         'USER': os.environ.get('DB_USER'),
         'PASSWORD':os.environ.get('DB_PASSWORD'),
